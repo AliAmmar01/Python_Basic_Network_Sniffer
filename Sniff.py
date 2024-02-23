cbdfb0efd46a5ba0ca60165f07a48cc64662d606
Ali Ammar <110249488+AliAmmar01@users.noreply.github.com>
2024-02-23 23:43:27 +0200

    Second Commit

--- Sniff.py
+++ Sniff.py
@@ -22,16 +22,23 @@ def main():
         result = subprocess.run(['ifconfig'], capture_output=True, text=True)
         print(result.stdout)    #To display your device interfaces
         interface = input("Select the interface you want to sniff on from the following option: ")  #Specify the interface
-        limit = int(input("Enter the number of packets to sniff: "))    #Specify the number of packets to stop sniffing after reaching
+        limit = int(input("Enter the number of packets to sniff (Enter 0 if you don't wish to sniff a specific number of packets): "))    #Specify the number of packets to stop sniffing after reaching
         answer = input("Do you wish to filter by a specific protocol? (Y/N) ")
-        
-        if answer == "Y" or answer == "y":
-            protocol=input("Specify the protocol: ")
-            sniff(filter=protocol,iface= interface, prn=PcktInfo, count=limit)  
+        if limit == 0:
+            if answer == "Y" or answer == "y":
+                protocol=input("Specify the protocol: ")
+                sniff(filter=protocol,iface= interface, prn=PcktInfo)  
+            else:
+                sniff(iface= interface, prn=PcktInfo)
         else:
-            sniff(iface= interface, prn=PcktInfo, count=limit)
+            if answer == "Y" or answer == "y":
+                protocol=input("Specify the protocol: ")
+                sniff(filter=protocol,iface= interface, prn=PcktInfo, count=limit)  
+            else:
+                sniff(iface= interface, prn=PcktInfo, count=limit)
 
-        #The sniff() is used for packet sniffing, 
+        #The sniff() is used for packet sniffing,
+        #filter=protocol specifies a filter for the packets to capture. It indicates the protocol or types of packets to capture.
         #iface=interface specifies the interface we want to sniff on (taken as input from the user),
         #prn=PcktInfo indicates that the function named PcktInfo will be called for each captured packet,
         #count=limit means that the packet sniffing will stop after capturing limit number of packets (taken as input from the user).
@@ -39,7 +46,6 @@ def main():
         exit = input("Do you want to exit? (Y/N) ")
         if exit == "Y" or exit == "y":
             break
-
 
 def PcktInfo(Packet):       #Callback Function used with sniff()
     print(Packet.show())    #Display detailed information about a packet including protocol-specific details.
